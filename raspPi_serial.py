7b83147af12d77eca4b1cb65d792c81d5e2b49a2
Ruth Deemua <130915232+RuthiezGit@users.noreply.github.com>
2023-07-12 10:31:41 +0100

    Add files via upload

--- raspPi_serial.py
+++ raspPi_serial.py
@@ -1,19 +1,52 @@
-import Rpi.GPIO as gpio
+import cv2
+# import Rpi.GPIO as gpio
 import serial
-import time
+# import time
 
-
-#initalize or open the serial port
 py_ser = serial.Serial("COM15", 9600, timeout = 1)
-while True:
-    # x = py_ser.read() #with empty argument
-    # s = py_ser.read(10) #with an existing or filled argument but takes only one byte, i.e 1, 2, 3,4 etc
-    # line = py_ser.readline() #to read a line of argument
-      
-    command = input("")
-    py_ser.write(str.encode(command))
-    time.sleep(0.5)
+cascade=cv2.CascadeClassifier(r'C:\Users\USER\Downloads\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
 
-    line = py_ser.readline()
-    print(line)
+cv2.namedWindow("preview")
+web_cam = cv2.VideoCapture(0)
 
+while web_cam.isOpened():
+    rval, frame =web_cam.read()
+    width, height, ch = frame.shape
+    center_frame = (width/2), (height/2)
+    if rval:
+        faces = cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5)
+        frame = cv2.rectangle(frame, (x,y), (x+w,y+h), color=(255,0,0), thickness=1)
+
+        x,y,w,h=faces[0]
+        center_obj = ((x+w)/2), ((y+h)/2)
+        area_obj = ((x+w)*(y+h))
+        area_desired = (width-1000)*(height-1000)
+
+        a,b = center_frame
+        c,d = center_obj
+        if (c < a-20):
+            # print("L")
+            py_ser.write(str.encode("L"))
+        elif (c > a+20):
+            # print("R")
+            py_ser.write(str.encode("R"))
+        
+        if (area_obj > area_desired):
+            # print("B")
+            py_ser.write(str.encode("B"))
+        elif (area_obj < area_desired):
+            # print("F")
+            py_ser.write(str.encode("F"))
+
+        cv2.imshow('preview', frame)
+        key =cv2.waitKey(2)
+        if key ==27:
+            break
+
+web_cam.release()
+cv2.destroyWindow("preview")
+
+cv2.imshow('picture', web_cam)
+
+line = py_ser.readline()
+print(line)
